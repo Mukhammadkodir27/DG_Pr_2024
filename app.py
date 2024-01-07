5ee4c883c5e7e3d7feb61f3e5c7aaabefa8f662c
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-01-07 04:14:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -6,7 +6,14 @@ app = Flask(__name__)
 def add():
     return render_template("add.html")
 
-
+@app.route("/addexpense", methods=["POST"])
+def addexpense(): 
+   date = request.form["date"]
+   expensename = request.form["expensename"]
+   amount = request.form["amount"]
+   category = request.form["category"]
+   print(date+""+expensename+""+amount+""+category)
+   return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True)
