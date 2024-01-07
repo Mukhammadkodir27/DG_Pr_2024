50d16e46d991f5757e59bbed405e797d19170fda
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-01-07 06:57:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,35 @@ class Expense(db.Model):
 def add():
     return render_template("add.html")
 
+@app.route("/delete/<int:id>")
+def delete(id):
+    expense = Expense.query.filter_by(id=id).first()
+    db.session.delete(expense)
+    db.session.commit()
+    return redirect("/expenses")
+
+@app.route("/updateexpense/<int:id>")
+def updateexpense(id):
+    expense = Expense.query.filter_by(id=id).first()
+    return render_template("updateexpense.html", expense=expense)
+
+@app.route("/edit", methods=["POST"])
+def edit():
+    id = request.form["id"]
+    date = request.form["date"]
+    expensename = request.form["expensename"]
+    amount = request.form["amount"]
+    category = request.form["category"]
+
+    expense = Expense.query.filter_by(id=id).first()
+    expense.date = date
+    expense.expensename = expensename
+    expense.amount = amount
+    expense.category = category
+
+    db.session.commit() 
+    return redirect("/expenses")
+
 @app.route("/expenses")
 def expenses():
     expenses = Expense.query.all()
