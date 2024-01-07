76f917e56687ad846dc616e5be9c42339e90d95d
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-01-07 05:16:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,36 @@
 from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+import os
 
-app = Flask(__name__)
+# def create_app():
+#     app = Flask(__name__)
+#     project_dir = os.path.dirname(os.path.abspath(__file__))
+#     database_file = "sqlite:///{}".format(os.path.join(project_dir, "mydatabase.db"))
+#     app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+#     db.init_app(app)
+#     return app
 
-@app.route("/")
+# db = SQLAlchemy()
+# app = create_app() 
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "mydatabase.db"))
+
+
+app = Flask(__name__)   
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+db = SQLAlchemy(app)
+
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(50), nullable=False)
+    expensename = db.Column(db.String(50), nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+
+
+@app.route("/") 
 def add():
     return render_template("add.html")
 
@@ -13,6 +41,9 @@ def addexpense():
    amount = request.form["amount"]
    category = request.form["category"]
    print(date+""+expensename+""+amount+""+category)
+   expense = Expense(date=date, expensename=expensename, amount=amount, category=category)
+   db.session.add(expense)
+   db.session.commit()
    return redirect("/")
 
 if __name__ == '__main__':
