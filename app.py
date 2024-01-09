a8ac38641be90ccec5eec0584a5aa19e4cbc852f
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-01-09 07:02:51 +0100

    corrected str + int

--- app.py
+++ app.py
@@ -51,22 +51,27 @@ def edit():
 
 @app.route("/expenses")
 def expenses():
-    expenses = Expense.query.all()
-    total = 0
-    t_business = 0
-    t_other = 0
-    t_food = 0
-    t_entertainment = 0 
-    for expense in expenses:
-        total += expense.amount
-        if expense.category == "business":
-           t_business += expense.amount
-        elif expense.category == "other":
-           t_other += expense.amount
-        elif expense.category == "food":
-           t_food += expense.amount
-        elif expense.category == "entertainment":
-           t_entertainment += expense.amount
+    try:
+       expenses = Expense.query.all()
+       total = 0
+       t_business = 0
+       t_other = 0
+       t_food = 0
+       t_entertainment = 0 
+       for expense in expenses:
+           total += expense.amount
+           if expense.category == "business":
+              t_business += expense.amount
+           elif expense.category == "other":
+              t_other += expense.amount
+           elif expense.category == "food":
+              t_food += expense.amount
+           elif expense.category == "entertainment":
+              t_entertainment += expense.amount
+    except TypeError as e:
+       # Handle the TypeError by printing a message or any other action
+       print(f"Error: {e}. Skipping the operation.")
+       
            
     return render_template("expenses.html", expenses=expenses,
                            total=total, t_business= t_business, t_entertainment= t_entertainment,
